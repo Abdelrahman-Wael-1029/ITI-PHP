b4558bb198d70c2002b4798fb58b0352c38b3233
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2023-07-21 12:20:07 +0300

    style form

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+window.onload = () => {
+  let skills = document.querySelectorAll("form>.skills>div>.allSkills");
+  let spans = document.querySelectorAll("form>.skills>div>span");
+  let mx = 0;
+  skills.forEach((Element) => (mx = Math.max(mx, Element.clientWidth)));
 
+  skills.forEach((Element) => (Element.style.width = mx + "px"));
+
+  spans.forEach((Element) => (mx = Math.max(mx, Element.clientWidth)));
+  spans.forEach((Element) => (Element.style.width = mx + "px"));
+};
